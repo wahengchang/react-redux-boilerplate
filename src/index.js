c38295a1652b5f49db1dbc0ba6b05e09880bfd29
waheng <peterchang@yahoo-inc.com>
2017-05-06 18:58:19 +0800

    Added redux-thunk

--- src/index.js
+++ src/index.js
@@ -3,11 +3,19 @@ import 'babel-polyfill'
 import React from 'react'
 import { render } from 'react-dom'
 import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import { createStore, compose, applyMiddleware } from 'redux'
 import helloReducer from './reducers'
 import App from './components/App'
+import thunk from 'redux-thunk'
 
-let store = createStore(helloReducer)
+const composeEnhancers = process.env.NODE_ENV !== 'production' && typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+  : compose
+
+const store = createStore(
+    helloReducer,
+    composeEnhancers(applyMiddleware(thunk))
+  )
 
 render(
   <Provider store={store}>

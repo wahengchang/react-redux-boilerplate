b90283695d1cf0373c2f6f28e3d2ec1348486f46
waheng <peterchang@yahoo-inc.com>
2017-06-10 21:53:37 +0800

    Update webpack1 -> webpack2

--- server.js
+++ server.js
@@ -8,11 +8,25 @@ var port = 3000
 if (process.env.NODE_ENV === 'development') {
   var webpack = require('webpack')
   var config = require('./webpack.config')
-  var webpackDevMiddleware = require('webpack-dev-middleware')
-  var webpackHotMiddleware = require('webpack-hot-middleware')
+
+  var devMiddleware = require('webpack-dev-middleware')
+  var hotDevMiddleware = require('webpack-hot-middleware')
   var compiler = webpack(config)
-  app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
-  app.use(webpackHotMiddleware(compiler))
+  var devMiddlewareConfig = {
+    noInfo: true,
+    stats: {colors: true},
+    publicPath: config.output.publicPath
+  }
+
+  app.use(devMiddleware(compiler, devMiddlewareConfig))
+  app.use(hotDevMiddleware(compiler))
+
+
+  // var webpackDevMiddleware = require('webpack-dev-middleware')
+  // var webpackHotMiddleware = require('webpack-hot-middleware')
+  // var compiler = webpack(config)
+  // app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
+  // app.use(webpackHotMiddleware(compiler))
 }
 
 app.use(require('express').static('public'))
--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,35 @@
-var path = require('path')
-var webpack = require('webpack')
+const { resolve, join } = require('path')
+const webpack = require('webpack')
 
-module.exports = {
-  devtool: 'cheap-module-eval-source-map',
-  entry: [
-    'webpack-hot-middleware/client',
-    './src/clientRender.js'
-  ],
+const config = {
+  devtool: 'cheap-eval-source-map',
+  context: resolve(__dirname, 'src'),
+  entry: {
+    home: [
+      'webpack-hot-middleware/client',
+      './clientRender.js'
+    ]
+  },
   output: {
-    path: path.join(__dirname, 'public/static'),
-    filename: 'bundle.js',
-    publicPath: '/static'
+    path: resolve(__dirname,'public/static'),
+    filename: '[name].js',
+    publicPath: '/static/'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          'babel-loader'
+        ],
+        exclude: '/node_modules/'
+      }
+    ]
   },
   plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin()
-  ],
-  module: {
-    loaders: [{
-      test: /\.js$/,
-      loaders: ['babel'],
-      exclude: /node_modules/,
-      include: __dirname
-    }]
-  }
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
 }
+module.exports = config
